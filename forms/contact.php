d1ba306d109beced9b7af738eb2b9bbe53305df4
Mee-chan <imeeatibagos14@gmail.com>
2023-10-11 10:06:08 +0800

    Modify file

--- forms/contact.php
+++ forms/contact.php
@@ -7,9 +7,9 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'imeeatibagos14@gmail.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/myportfolio' )) {
     include( $php_email_form );
   } else {
     die( 'Unable to load the "PHP Email Form" Library!');
@@ -23,19 +23,20 @@
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct 
   /*
   $contact->smtp = array(
     'host' => 'example.com',
     'username' => 'example',
     'password' => 'pass',
     'port' => '587'
-  );
-  */
+  );*/
+
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
   echo $contact->send();
+  $contact->honeypot = $_POST['first_name'];
 ?>
